a32e4942881f6c9e012467bb050d0cbd0f9a9d48
john-h-celeste <rvail8330@floridapoly.edu>
2025-12-02 20:14:50 +0000

    i love doing coding challenges <3

---
+++ aoc2.py
@@ -0,0 +1,32 @@
+import math
+
+inp = '67562556-67743658,62064792-62301480,4394592-4512674,3308-4582,69552998-69828126,9123-12332,1095-1358,23-48,294-400,3511416-3689352,1007333-1150296,2929221721-2929361280,309711-443410,2131524-2335082,81867-97148,9574291560-9574498524,648635477-648670391,1-18,5735-8423,58-72,538-812,698652479-698760276,727833-843820,15609927-15646018,1491-1766,53435-76187,196475-300384,852101-903928,73-97,1894-2622,58406664-58466933,6767640219-6767697605,523453-569572,7979723815-7979848548,149-216'
+
+[print(len(r.split('-'))) for r in inp.split(',')]
+
+bounds = [(int(a), int(b)) for r in inp.split(',') for a,b in [r.split('-')]]
+print(bounds)
+
+top = max([n for r in bounds for n in r])
+topi = int(str(top)[:math.ceil(len(str(top)) / 2)]) + 1
+print(topi)
+
+ranges = [range(a, b + 1) for a,b in bounds]
+
+possible = [int(str(i) * 2) for i in range(topi)]
+
+existing = [n for n in possible if any(n in r for r in ranges)]
+print(sum(existing))
+
+topi = int(str(top)[:math.ceil(len(str(top)) / 2)]) + 1
+print(topi)
+
+possible = [int(str(i) * n) for n in range(2, len(str(top)) + 1) for i in range(int(str(top)[:math.ceil(len(str(top)) / n)]) + 1)]
+
+existing = [n for n in possible if any(n in r for r in ranges)]
+print(sum(existing))
+
+possible = {*possible}
+
+existing = [n for n in possible if any(n in r for r in ranges)]
+print(sum(existing))
